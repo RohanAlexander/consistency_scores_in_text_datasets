02d83fb2d3b44b4d9ad71afdb8f77ab220dcd60e
kelichiu <keli.chiu.kc@gmail.com>
2020-08-26 08:07:12 -0400

    BERT implementation in R updated

--- scripts/00-try-BERT-text-prediction.R
+++ scripts/00-try-BERT-text-prediction.R
@@ -2,53 +2,70 @@
 Sys.setenv(TF_KERAS=1) 
 # to see python version
 reticulate::py_config()
-setwd("~/Desktop/repos/consistency_scores_in_text_datasets/model starters")
+setwd("~/Desktop/repos/consistency_scores_in_text_datasets/model_starters")
 
-# Read text from image
+#### 1. Read text from image
 library(tesseract)
 eng <- tesseract("eng")
-text_original <- tesseract::ocr("BERT Python/image1.png", engine = eng)
+text_original <- tesseract::ocr("BERT_Python/image1.png", engine = eng)
 cat(text_original)
 
-# Collect incorrect words
+#### 2. Collect incorrect words
 library(textclean)
 text_stripped <- strip(text_original, char.keep = c("?", ".", "’", "~~"), digit.remove = TRUE, apostrophe.remove = FALSE,
-      lower.case = FALSE)
+                       lower.case = FALSE)
+
 library(hunspell)
 incorrectwords <- hunspell(text_stripped)
 
-# Configure paths to the pre-trained files variables
-pretrained_path = 'uncased_L-12_H-768_A-12'
+tokens <- unlist(strsplit(text_stripped, "[^aA-zZ<>/]"))
+mask_indices <- match(c("imlfon", "Kism"),tokens)
+tokens[12] = tokens[33] = "[MASK]"
+toString(tokens)
+#### 3. Configure paths to the BERT pre-trained files variables
+# The zip file is available through the tutorial link at the first line
+pretrained_path = '../../../uncased_L-12_H-768_A-12'
 config_path = file.path(pretrained_path, 'bert_config.json')
 checkpoint_path = file.path(pretrained_path, 'bert_model.ckpt')
 vocab_path = file.path(pretrained_path, 'vocab.txt')
 seq_length = NULL
 
-# Load BERT and tokenize the text
+#### 4. Load BERT and tokenize the text
 library(reticulate)
 k_bert = import('keras_bert')
+np = import('numpy')
 token_dict = k_bert$load_vocabulary(vocab_path)
 tokenizer = k_bert$Tokenizer(token_dict)
-encodes <- tokenizer$encode(text_original)
 model = k_bert$load_trained_model_from_checkpoint(
   config_path,
   checkpoint_path,
   seq_len=seq_length,
   training=T)
 
-tokens <- tokenizer$tokenize(text_original)
-length(tokens)
+library(zeallot)
+#### 5. Tokenize the text and get the encoding
+tokens <- tokenizer$tokenize(toString(tokens))
+encodes <- tokenizer$encode(toString(tokens))
 
-# Get encoded inputs for the model: train, segments, targets
-indices = encodes[1]
-segments = encodes[2]
-masks = list(list(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0))
+#### 6. Get encoded inputs for the model: train, segments, targets
+indices <- encodes[1]
+indices <- as.matrix(indices)
+segments <- encodes[2]
+segments <- as.matrix(segments)
+## Manually flag word 13 and word 33 as masked words to be predicted 
+masks <- as.matrix(list(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0))
+train <- indices
+targets <- masks
+
 train = do.call(cbind,indices) %>% t()
 segments = do.call(cbind,segments) %>% t()
 targets = do.call(cbind,masks) %>% t()
 
-# Get the prediction matrix
-prediction <- model$predict(c(train))
+#### 7. Get the prediction matrix
+prediction <- model$predict(list(train,segments,targets))
+predictions <- np$argmax(prediction[1], axis=-1L)
+
+# 8. Use the index provided by prediction matrix to look for the predicted word from the token_dict
 
